bce42067d86afc517cde943657abb40a910aa0a8
Julia Mertz <info@juliamertz.dev>
2024-07-14 22:12:26 +0200

    x

--- system/apps/home-assistant.nix
+++ system/apps/home-assistant.nix
@@ -5,8 +5,18 @@ let
 in {
   environment.systemPackages = [ pkg ];
   services.home-assistant = {
-    enable = true;    
+    enable = true;
     configDir = /home/${settings.user.username}/home-assistant;
-    config = { };
-  }; 
+    extraComponents = [
+      # Components required to complete the onboarding
+      "esphome"
+      "met"
+      "radio_browser"
+    ];
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = {};
+    };
+  };
 }
