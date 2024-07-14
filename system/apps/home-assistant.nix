97d2e2550d9c4ca986365b1456253bbd163039c1
Julia Mertz <info@juliamertz.dev>
2024-07-14 22:07:49 +0200

    fix?

--- system/apps/home-assistant.nix
+++ system/apps/home-assistant.nix
@@ -1,6 +1,9 @@
-{ pkgs, lib, settings, ... }:
-{
-  environment.systemPackages = with pkgs; [ home-assistant ];
+{ settings, inputs, ... }:
+let
+  platform = settings.system.platform;
+  pkg = inputs.nixpkgs-24_05.legacyPackages.${platform}.sunshine;
+in {
+  environment.systemPackages = [ pkg ];
   services.home-assistant = {
     enable = true;    
     configDir = /home/${settings.user.username}/home-assistant;
