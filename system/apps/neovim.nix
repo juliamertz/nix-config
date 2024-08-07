05c5972242e089ea5be4ff7051fded0516eef178
Julia Mertz <info@juliamertz.dev>
2024-08-07 22:02:28 +0200

    fix vim config

--- system/apps/neovim.nix
+++ system/apps/neovim.nix
@@ -5,17 +5,8 @@ let
     package = pkgs.neovim;
     dependencies = with pkgs; [ ripgrep stdenv.cc nixfmt ];
     extraFlags = "-u ${dotfiles.path}/nvim/init.lua";
-    # preWrap = # sh
-    #   ''
-    #     # Enable dynamically linked mason lsp binaries
-    #     NIX_LD=$(<${pkgs.stdenv.cc}/nix-support/dynamic-linker)
-    #   '';
-    # extraArgs =
-    #   [ "--set NIX_LD $NIX_LD" "--set XDG_CONFIG_HOME '${dotfiles.path}'" ];
-    postWrap = # sh
-      ''
-        ln -sf $out/bin/nvim $out/bin/vim
-      '';
+    extraArgs = [ "--set XDG_CONFIG_HOME '${dotfiles.path}'" ];
+    postWrap = /*sh*/ '' ln -sf $out/bin/nvim $out/bin/vim '';
   };
 in {
   environment.systemPackages = [ nvim ];
