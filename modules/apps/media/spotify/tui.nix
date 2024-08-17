e650cde2c4f43cb9c0ebae960ab438dab208bf5c
Julia Mertz <info@juliamertz.dev>
2024-08-17 17:17:45 +0200

    update spotify-tui module

--- modules/apps/media/spotify/tui.nix
+++ modules/apps/media/spotify/tui.nix
@@ -1,34 +1,30 @@
 { lib, inputs, dotfiles, helpers, settings, ... }:
 let
-  user = user.settings.username;
+  user = settings.user.username;
   pkgs = inputs.nixpkgs-unstable.legacyPackages.${settings.system.platform};
-  base = pkgs.spotify-player.override {
+
+  featureFlags = {
     withAudioBackend = "alsa";
     withSixel = false;
     withNotify = false;
     withLyrics = false;
   };
-
-  overlay = base.overrideAttrs (old: {
-    name = "spotify_player";
-    src = pkgs.fetchFromGitHub {
-      owner = "juliamertz";
-      repo = "spotify-player";
-      rev = "50dcde62f933ef3da251db914eb71ad89a3566b0";
-      sha256 = "sha256-I03hQWgCUiXL9v46mlkW9T2hjARoxIMZEgz7V0HHcts=";
-    };
-    hash = "";
-    cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
-      name = "${base.name}-vendor.tar.gz";
-      src = base.src;
-    });
-  });
-
-  # sops.secrets = helpers.ownedSecrets user [ "spotify_client_id" ];
-
-  sops.secrets = {
-    spotify_client_id = { owner = settings.user.username; };
-  };
+  base = pkgs.spotify-player.override featureFlags;
+  overlay = base.overrideAttrs (old:
+    {
+      name = base.name;
+      src = pkgs.fetchFromGitHub {
+        owner = "juliamertz";
+        repo = "spotify-player";
+        rev = "50dcde62f933ef3da251db914eb71ad89a3566b0";
+        sha256 = "sha256-I03hQWgCUiXL9v46mlkW9T2hjARoxIMZEgz7V0HHcts=";
+      };
+      hash = "";
+      cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+        name = "${base.name}-vendor.tar.gz";
+        src = base.src;
+      });
+    } // featureFlags);
 
   wrapped = helpers.wrapPackage {
     package = overlay;
@@ -39,5 +35,9 @@ let
         ln -sf $out/bin/spotify_player $out/bin/spt
       '';
   };
-in { environment.systemPackages = [ wrapped ]; }
+in {
+  sops.secrets = helpers.ownedSecrets user [ "spotify_client_id" ];
+
+  environment.systemPackages = [ wrapped ];
+}
 
