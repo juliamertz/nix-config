344ed6c1f6fe5abf7b09300fdb1117594cf666b5
Julia Mertz <info@juliamertz.dev>
2024-08-28 22:48:50 +0200

    move git config to homemanager

--- modules/apps/git.nix
+++ modules/apps/git.nix
@@ -1,37 +1,39 @@
-{ settings, ... }: let
-  inherit (settings) user;
-in{
+{ settings, ... }:
+let inherit (settings) user;
+in {
   home.programs.git = {
     enable = true;
     userName = user.fullName;
     userEmail = user.email;
+
+    aliases = {
+      s = "status";
+      a = "add";
+      c = "commit -m";
+
+      wa = "worktree add";
+      wr = "worktree remove";
+      wl = "worktree list";
+    };
+
+    extraConfig =
+      # ini
+      ''
+        [core]
+        editor = "nvim"
+
+        [init]
+        defaultBranch = "main"
+
+        [pull]
+        rebase = true
+
+        [url "git@github.com:juliamertz/"]
+        insteadOf = "julia:"
+
+        [url "https://github.com/"]
+        insteadOf = "gh:"
+        insteadOf = "github:"
+      '';
   };
-
-  # programs.git = {
-  #   enable = true;
-  #   config = {
-  #     user = {
-  #       name = settings.user.fullName;
-  #       inherit (settings.user) email;
-  #     };
-  #     init.defaultBranch = "main";
-  #     core.editor = settings.user.editor;
-  #     pull.rebase = true;
-  #     url = {
-  #       "https://github.com/" = { insteadOf = [ "gh:" "github:" ]; };
-  #       "git@github.com:juliamertz/" = { insteadOf = [ "julia:" ]; };
-  #     };
-  #     alias = {
-  #       s = "status";
-  #       a = "add";
-  #       c = "commit -m";
-  #
-  #       wa = "worktree add";
-  #       wr = "worktree remove";
-  #       wl = "worktree list";
-  #     };
-  #   };
-  #
-  # };
-
 }
