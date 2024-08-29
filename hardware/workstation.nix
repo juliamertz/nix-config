448827918fc088c852935b2ad019d4aa8eeb2bba
Julia Mertz <info@juliamertz.dev>
2024-08-29 03:29:26 +0200

    update hw config

--- hardware/workstation.nix
+++ hardware/workstation.nix
@@ -1,15 +1,19 @@
-{ config, inputs, settings, ... }:
+{ config, inputs, settings, lib, pkgs, modulesPath, ... }:
 let
   pkgs = import inputs.nixpkgs-unstable {
     system = settings.system.platform;
     config.allowUnfree = true;
   };
-
 in {
-  # Graphics
   nixpkgs.config.packageOverrides = pkgs: {
     inherit (pkgs) linuxPackages_latest nvidia_x11;
   };
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   environment.systemPackages = with pkgs; [ cudatoolkit ];
@@ -19,20 +23,44 @@ in {
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    # Set nvidia-drm.modeset=1 kernel parameter
     modesetting.enable = true;
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     powerManagement = {
       enable = true;
-      # Fine-grained power management. Turns off GPU when not in use.
-      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
       finegrained = false;
     };
-    open = false; # Use the NVidia open source kernel module
+    open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
-  # Networking
+  networking.useDHCP = lib.mkDefault true;
   networking.interfaces.enp4s0 = { wakeOnLan.enable = true; };
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/7a3e2cc9-a8cd-4c05-ba11-92fc603488e4";
+    fsType = "ext4";
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/2e75cdd2-6cec-47cb-951d-f88101e80b31";
+    fsType = "ext4";
+  };
+
+  fileSystems."/games" = {
+    device = "/dev/disk/by-uuid/f32a0b81-efbf-4696-b421-50331ec2b2b4";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/2D88-45C8";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
+
+  swapDevices = [ ];
 }
