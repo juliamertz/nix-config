3344b5adfde4591f1a4673d220a17da302af6dd9
Julia Mertz <info@juliamertz.dev>
2024-08-30 22:17:14 +0200

    shared base config

--- profiles/base.nix
+++ profiles/base.nix
@@ -1,4 +1,4 @@
-{ pkgs, settings, inputs, ... }:
+{ pkgs, settings, inputs, helpers, ... }:
 let user = settings.user.username;
 in {
   imports = [
@@ -6,43 +6,56 @@ in {
     inputs.flake-programs-sqlite.nixosModules.programs-sqlite
   ];
 
-  environment.systemPackages = with pkgs; [ openssl curl tldr xclip zip unzip ];
+  config = mkMerge [
+    (if helpers.isDarwin then
+      {
+        # Darwin defaults
+      }
+    else {
+      # Nixos defaults stuff
+      environment.systemPackages = with pkgs; [ xclip ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      networking.firewall.enable = true;
+      networking.networkmanager.enable = true;
 
-  networking.hostName = settings.system.hostname;
-  networking.firewall.enable = true;
-  networking.networkmanager.enable = true;
+      users.users.${user} = {
+        isNormalUser = true;
+        description = settings.user.fullName;
+        extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+      };
 
-  users.users.${user} = {
-    isNormalUser = true;
-    description = settings.user.fullName;
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
-  };
+      time.timeZone = settings.system.timeZone;
+      i18n.defaultLocale = settings.system.defaultLocale;
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "nl_NL.UTF-8";
+        LC_IDENTIFICATION = "nl_NL.UTF-8";
+        LC_MEASUREMENT = "nl_NL.UTF-8";
+        LC_MONETARY = "nl_NL.UTF-8";
+        LC_NAME = "nl_NL.UTF-8";
+        LC_NUMERIC = "nl_NL.UTF-8";
+        LC_PAPER = "nl_NL.UTF-8";
+        LC_TELEPHONE = "nl_NL.UTF-8";
+        LC_TIME = "nl_NL.UTF-8";
+      };
 
-  time.timeZone = settings.system.timeZone;
-  i18n.defaultLocale = settings.system.defaultLocale;
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nl_NL.UTF-8";
-    LC_IDENTIFICATION = "nl_NL.UTF-8";
-    LC_MEASUREMENT = "nl_NL.UTF-8";
-    LC_MONETARY = "nl_NL.UTF-8";
-    LC_NAME = "nl_NL.UTF-8";
-    LC_NUMERIC = "nl_NL.UTF-8";
-    LC_PAPER = "nl_NL.UTF-8";
-    LC_TELEPHONE = "nl_NL.UTF-8";
-    LC_TIME = "nl_NL.UTF-8";
-  };
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+      programs.nh = {
+        enable = true;
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 4d --keep 3";
+        flake = "/home/${user}/nix";
+      };
 
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/${user}/nix";
-  };
+      system.stateVersion = "24.05";
+    })
+    # Shared defaults
+    {
+      environment.systemPackages = with pkgs; [ openssl curl tldr zip unzip ];
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  system.stateVersion = "24.05";
+      networking.hostName = settings.system.hostname;
+    }
+  ];
 }
