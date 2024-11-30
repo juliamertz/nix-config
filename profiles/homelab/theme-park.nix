2adce5aaae62b46c03570a34178df1c391c49938
Julia Mertz <info@juliamertz.dev>
2024-11-30 20:22:19 +0100

    update theme park

--- profiles/homelab/theme-park.nix
+++ profiles/homelab/theme-park.nix
@@ -11,7 +11,8 @@ let
   themepark =
     let
       repo = "juliamertz/theme.park";
-      rev = "cdedb571787abc785e4bf4a24449f20a3d492370";
+      hash = "sha256-KEDiaowklp5Bi/N5ak1ZCmWGiLSnCkYvQLM0PAWZFq0=";
+      rev = "0f99c377e6a41dd90c12e007eacbae18a36c5286";
     in
     (builtins.getFlake "github:${repo}/${rev}?dir=nix").packages.${pkgs.system}.default;
 in
