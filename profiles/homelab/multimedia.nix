747cdf9f1ae36c190b8f9bec2b41823ae70fa2d8
Julia Mertz <info@juliamertz.dev>
2025-01-01 19:00:13 +0100

    pin jackett & sonarr

--- profiles/homelab/multimedia.nix
+++ profiles/homelab/multimedia.nix
@@ -8,7 +8,17 @@
 }:
 let
   cfg = config.jellyfin;
-  unstable = helpers.getPkgs inputs.nixpkgs-unstable;
+  # specific version where radarr/sonarr/jackett don't have weird dotnet requirements.
+  pkgs =
+    import
+      (builtins.fetchTarball {
+        name = "nixpkgs-unstable-dotnet";
+        url = "https://github.com/nixos/nixpkgs/archive/5e4fbfb6b3de1aa2872b76d49fafc942626e2add.tar.gz";
+        sha256 = "127dk2l5fr0922an96rmlzg32vh6pxh7r6qyvkhwf20jdzg9k61r";
+      })
+      {
+        system = settings.system.platform;
+      };
 in
 {
   imports = [ ../../modules/containers/jellyfin.nix ];
@@ -29,7 +39,7 @@ in
 
     nixpkgs.config = lib.mkIf cfg.enableTorrent {
       packageOverrides = _: {
-        inherit (unstable)
+        inherit (pkgs)
           jellyseerr
           radarr
           sonarr
