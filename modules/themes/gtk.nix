70643f624c398c8febc7b26dfd24647b3fd9ea28
Julia Mertz <info@juliamertz.dev>
2025-01-31 12:01:21 +0100

    fix gtk theme
    cleanup gtk theme

---
+++ modules/themes/gtk.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.gtk;
+in
+{
+  options.gtk = with lib; {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+    };
+
+    settings = {
+      theme-name = mkOption {
+        type = types.str;
+        default = "Adwaita";
+      };
+      icon-theme-name = mkOption {
+        type = types.str;
+        default = "Adwaita";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.etc."xdg/gtk-2.0/gtkrc".text = ''
+      gtk-theme-name=${cfg.settings.theme-name}
+      gtk-icon-theme-name=${cfg.settings.icon-theme-name}
+    '';
+
+    environment.etc."xdg/gtk-3.0/settings.ini".text = ''
+      [Settings]
+      gtk-theme-name=${cfg.settings.theme-name}
+      gtk-icon-theme-name=${cfg.settings.icon-theme-name}
+    '';
+  };
+}
--- modules/themes/rose-pine/default.nix
+++ modules/themes/rose-pine/default.nix
@@ -1,62 +1,24 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
-}:
+{ pkgs, ... }:
 let
-  cfg = config.rose-pine;
   colors = pkgs.callPackage ./colors.nix { };
 in
 {
-  options = {
-    rose-pine.variant = lib.mkOption {
-      type = lib.types.str;
-      default = "moon";
-    };
+  stylix.image = ./bg.jpeg;
+  stylix.enable = true;
+  stylix.polarity = "dark";
+  stylix.base16Scheme = colors.moon;
+
+  home.config.stylix.targets.gtk.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    rose-pine-gtk-theme
+    rose-pine-icon-theme
+  ];
+
+  gtk.settings = {
+    theme-name = "rose-pine-moon";
+    icon-theme-name = "rose-pine-moon";
   };
 
-  config = {
-    stylix.image = ./bg.jpeg;
-    stylix.enable = true;
-    stylix.polarity = "dark";
-    stylix.base16Scheme = colors.${cfg.variant};
-
-    environment.systemPackages = with pkgs; [
-      rose-pine-gtk-theme
-      rose-pine-icon-theme
-      libsForQt5.qtstyleplugin-kvantum
-      libsForQt5.qt5ct
-    ];
-
-    home.config.stylix.targets.gtk.enable = false;
-    home.file.".config/gtk-2.0/gtkrc".text = ''
-      gtk-theme-name = rose-pine-moon
-      gtk-icon-theme-name = oomox-rose-pine-moon
-    '';
-    home.file.".config/gtk-3.0/settings.ini".text = 
-    # ini
-    ''
-      [Settings]
-      gtk-theme-name=rose-pine-moon 
-      gtk-icon-theme-name=rose-pine-moon                                                                                      
-      gtk-font-name=Sans 10
-    '';
-
-    qt = {
-      enable = true;
-      platformTheme = "gtk2";
-      style = "adwaita";
-    };
-
-    nixpkgs.config.qt5 = {
-      enable = true;
-      platformTheme = "qt5ct";
-      style = {
-        package = pkgs.utterly-nord-plasma;
-        name = "Utterly Nord Plasma";
-      };
-    };
-
-  };
+  imports = [ ../gtk.nix ];
 }
