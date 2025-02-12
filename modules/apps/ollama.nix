2d6208328046448f45da98b28d17770f1646f0ac
Julia Mertz <info@juliamertz.dev>
2025-02-13 00:39:20 +0100

    cleanup ollama config

--- modules/apps/ollama.nix
+++ modules/apps/ollama.nix
@@ -1,23 +1,16 @@
-{ inputs, settings, ... }:
-let
-  pkgs = import inputs.nixpkgs-unstable {
-    system = settings.system.platform;
-    config.allowUnfree = true;
-  };
-in
 {
-  # systemd.tmpfiles.rules = [
-  #   "d /home/${user} 0775 ${user} ${group}"
-  # ];
-
+  pkgs,
+  ...
+}:
+{
   services.ollama = {
     enable = true;
-    # inherit user group;
     user = "ollama";
     group = "ollama";
 
-    package = pkgs.ollama;
-    acceleration = "cuda";
+    package = pkgs.ollama-cuda;
+    # this option causes ollama to be re-compiled which takes VERY long
+    # acceleration = "cuda";
 
     home = "/games/ollama";
     models = "/games/ollama/models";
