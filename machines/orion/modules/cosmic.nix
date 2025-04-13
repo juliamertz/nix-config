682fc8105d595968ff701118b59bbd3d21b02c6c
Julia Mertz <info@juliamertz.dev>
2025-04-13 19:08:30 +0200

    cleanup cosmic config

--- machines/orion/modules/cosmic.nix
+++ machines/orion/modules/cosmic.nix
@@ -2,18 +2,16 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  cosmic-comp = inputs.cosmic-comp.packages.${pkgs.system}.default.overrideAttrs {useXWayland = false;};
+in {
   imports = [inputs.cosmic.nixosModules.default];
 
   config = {
     services.desktopManager.cosmic.enable = true;
 
-    environment.cosmic.excludePackages = with pkgs; [
-      cosmic-comp
-    ];
-    environment.systemPackages = [
-      (inputs.cosmic-comp.packages.${pkgs.system}.default.overrideAttrs {useXWayland = false;})
-    ];
+    environment.cosmic.excludePackages = [pkgs.cosmic-comp];
+    environment.systemPackages = [cosmic-comp];
 
     nix.settings = {
       substituters = ["https://cosmic.cachix.org/"];
