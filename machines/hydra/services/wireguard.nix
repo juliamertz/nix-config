6fb67aa26c6256c3544d4b05c5e64cd78dbfd23f
Julia Mertz <info@juliamertz.dev>
2025-05-29 17:44:28 +0200

    update homelab wg config
    amend: fix range

--- machines/hydra/services/wireguard.nix
+++ machines/hydra/services/wireguard.nix
@@ -5,14 +5,14 @@ in {
 
   networking.wg-quick.interfaces = {
     wg0 = {
-      address = ["10.100.0.4/24"];
+      address = ["10.100.1.1/23"];
       privateKeyFile = "${settings.user.home}/wireguard/private";
 
       peers = [
         {
           publicKey = "+UMRNrDpies7uCO4wCgxKdyDuN1/FpmIilO8/NO66Uo=";
-          allowedIPs = ["10.100.0.0/24"];
-          endpoint = "116.203.24.1:51820";
+          allowedIPs = ["10.100.0.0/23"];
+          endpoint = "wg.juliamertz.nl:51820";
           persistentKeepalive = 25;
         }
       ];
