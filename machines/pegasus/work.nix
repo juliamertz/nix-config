be487ece7ea1b2384392f6a2e2ce074403640515
Julia Mertz <info@juliamertz.dev>
2025-08-14 01:32:06 +0200

    update

--- machines/pegasus/work.nix
+++ machines/pegasus/work.nix
@@ -7,7 +7,14 @@
     kubectl
     kubectl-cnpg
     kubelogin
-    aptakube
+    (aptakube.overrideAttrs (let
+      version = "1.12.4";
+    in {
+      src = fetchurl {
+        url = "https://github.com/aptakube/aptakube/releases/download/${version}/Aptakube_${version}_universal.dmg";
+        sha256 = "sha256-JGM8bapGBgQb1wa4HjJ1sa/iX8P95x/jw2wdibpqrw0=";
+      };
+    }))
   ];
 
   homebrew.casks = [
