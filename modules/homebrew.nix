618cbf95aad7984a7a5c28bc3ccc957096f80b7d
Julia Mertz <info@juliamertz.dev>
2025-10-16 23:01:53 +0200

    add macos cross toolchains brew tap

--- modules/homebrew.nix
+++ modules/homebrew.nix
@@ -1,8 +1,16 @@
 {
+  pkgs,
   inputs,
   settings,
   ...
-}: {
+}: let
+  crossToolchains = pkgs.fetchFromGitHub {
+    owner = "messense";
+    repo = "homebrew-macos-cross-toolchains";
+    rev = "2a0a4036993160f555a3df86a53a91a89a2701ac";
+    hash = "sha256-tNLvlVXHze5etN2K+p9OScm8v8zPWaTmSPrxARdQMRc=";
+  };
+in {
   imports = [inputs.nix-homebrew.darwinModules.nix-homebrew];
   homebrew.enable = true;
 
@@ -15,6 +23,7 @@
     taps = with inputs; {
       "homebrew/homebrew-core" = homebrew-core;
       "homebrew/homebrew-cask" = homebrew-cask;
+      "messense/homebrew-macos-cross-toolchains" = crossToolchains;
     };
   };
 }
