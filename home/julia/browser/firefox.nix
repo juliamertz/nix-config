5deb1ff7711e6fb7f0eb4899a1ad9b516052452e
Julia Mertz <info@juliamertz.dev>
2025-10-16 23:01:20 +0200

    fix: pin firefox

--- home/julia/browser/firefox.nix
+++ home/julia/browser/firefox.nix
@@ -1,15 +1,15 @@
 {
   inputs,
   pkgs,
-  lib,
-  helpers,
   ...
 }: let
+  pkgs-25_05 = import inputs.nixpkgs-25_05 {inherit (pkgs) system;};
   firefoxAddons = inputs.nur.packages.${pkgs.system}.firefoxAddons;
   profile = import ./profile.nix {inherit pkgs firefoxAddons;};
 in {
   programs.firefox = {
     enable = true;
+    package = pkgs-25_05.firefox;
     profiles.julia = profile;
   };
 
