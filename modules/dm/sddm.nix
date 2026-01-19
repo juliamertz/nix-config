8f2f49b7426231eda0ebb975b328ba1777e0c7a3
Julia Mertz <info@juliamertz.dev>
2026-01-19 23:35:53 +0100

    fix: sddm theme

--- modules/dm/sddm.nix
+++ modules/dm/sddm.nix
@@ -3,22 +3,24 @@
   lib,
   inputs,
   ...
-}: {
-  environment.systemPackages = [
-    inputs.nur.packages.${pkgs.stdenv.hostPlatform.system}.sddm-rose-pine
-  ];
+}: let
+  sddm-rose-pine = inputs.nur.packages.${pkgs.stdenv.hostPlatform.system}.sddm-rose-pine;
+in {
+  environment.systemPackages = [sddm-rose-pine];
 
   services.displayManager = {
     defaultSession = "none+awesome";
     sddm = {
       enable = true;
       theme = "rose-pine";
-      setupScript =
-        # bash
-        ''
-          # turn off left monitor
-          ${lib.getExe pkgs.xorg.xrandr} --output HDMI-0 --off
-        '';
+      extraPackages = [sddm-rose-pine];
     };
   };
+
+  services.xserver.displayManager.setupCommands =
+    # bash
+    ''
+      # turn off left monitor
+      ${lib.getExe pkgs.xorg.xrandr} --output HDMI-0 --off
+    '';
 }
